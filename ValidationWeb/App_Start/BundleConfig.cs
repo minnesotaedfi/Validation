f26ca644559ff196da3109c1ba377e539adfbaad
Eugene Bird <eugene.bird@doublelinepartners.com>
2018-05-25 14:18:44 -0500

    MOAI-25 Fixing Modal Dialogs

--- ValidationWeb/App_Start/BundleConfig.cs
+++ ValidationWeb/App_Start/BundleConfig.cs
@@ -7,17 +7,20 @@ namespace ValidationWeb
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Content/Scripts/jquery-{version}.js"));
-            bundles.Add(new ScriptBundle("~/bundles/popper").Include("~/Content/Scripts/popper.min.js"));
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Content/Scripts/modernizr-*"));
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Content/Scripts/bootstrapjs",
-                      "~/Content/Scripts/respond.js"));
+            // STYLE
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/Styles/bootstrap.css",
                       "~/Content/Styles/validationportal.css"));
+
+            // SCRIPTS
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Content/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Content/Scripts/jquery-{version}.js"));
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Content/Scripts/popper.min.js",
+                      "~/Content/Scripts/bootstrap.js",
+                      "~/Content/Scripts/respond.js"));
         }
     }
 }
