ac07d561e7f4295f1575c9f47255d9a3715ef562
DOUBLELINE\ebird <ebird@donotreply0.com>
2018-06-13 17:22:21 -0500

    UI changes, database additions

---
+++ ValidationWeb/Database/ValidationPortalDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ValidationWeb
+{
+    public partial class ValidationPortalDbContext : DbContext
+    {
+        // Use of "name=" ensures that a new database won't be created by default.
+        public ValidationPortalDbContext()
+            : base("name=ValidationPortalDbContext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ValidationPortalDbContext, ValidationPortalDbMigrationConfiguration>(true));
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        static ValidationPortalDbContext()
+        {
+            // Fixes a known bug in which EntityFramework.SqlServer.dll is not copied into consumer even when CopyLocal is True.
+            var includeSqlServerDLLInConsumer = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
+        //public virtual DbSet<Client> Clients { get; set; }
+        //public virtual DbSet<Token> Tokens { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
---
+++ ValidationWeb/Database/ValidationPortalDbMigrationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace ValidationWeb
+{
+    internal sealed class ValidationPortalDbMigrationConfiguration : DbMigrationsConfiguration<ValidationPortalDbContext>
+    {
+        public ValidationPortalDbMigrationConfiguration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "ValidationWeb.ValidationPortalDbContext";
+        }
+
+        protected override void Seed(ValidationPortalDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
+
