b78885156e73c8da4a2f78367fdf0b14a6104e5a
DOUBLELINE\ebird <ebird@donotreply0.com>
2018-11-26 02:14:59 -0600

    Allow multitenant ODS with rules engine

--- Engine/Models/Rule.cs
+++ Engine/Models/Rule.cs
@@ -12,7 +12,7 @@ namespace Engine.Models
         public List<string> Components { get; } = new List<string>();
         public string Sql { get; }
         public string ExecSql => $"INSERT INTO [{_schemaProvider.Value}].[RuleValidationDetail] " +
-                                 $"SELECT @RuleValidationId, * FROM ({Sql}) _sql WHERE {WhereCondition}";
+                                 $"SELECT @RuleValidationId, Id, RuleId, IsError, Message FROM ({Sql}) _sql WHERE {WhereCondition}";
         protected string WhereCondition { get; set; } = " (1 = 1) "; // Default is effectively NO filter.
 
         public Rule(string ruleId, string rulesetId, ISchemaProvider schemaProvider, IEnumerable<string> collectionIds = null, string sql = null, IEnumerable<string> components = null)
