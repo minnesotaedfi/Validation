86f1539c6e3ee6d805e519fcf46513b3caf10b51
Kyle Howard <45075811+khoward-dl@users.noreply.github.com>
2019-01-25 13:57:35 -0600

    Uploading unit test runner powershell

---
+++ runtests.ps1
@@ -0,0 +1,5 @@
+﻿nuget install nunit.runners -Version 2.6.3 -o ./packages -FallbackSource 'https://api.nuget.org/v3/index.json'
+
+$nunit = ".\packages\nunit.runners.2.6.3\tools\nunit-console.exe"
+
+& $nunit /noshadow /process="Multiple" /domain="Multiple" /framework:"net-4.0" /xml:Tests.nunit.xml ".\validationweb.tests\bin\Release\ValidationWeb.Tests.dll"
