b47af17b8a275a5ec1f1e430b51b76be9e5a1db9
Pocky Southmayd <j.southmayd@wearedoubleline.com>
2019-02-05 17:59:56 -0600

    UI compatibility for IE11. admin is only section guaranteed to work

--- ValidationWeb/App_Start/BundleConfig.cs
+++ ValidationWeb/App_Start/BundleConfig.cs
@@ -9,9 +9,8 @@ namespace ValidationWeb
         {
             // STYLE
             bundles.Add(new StyleBundle("~/Content/Styles/css").Include(
-                      "~/Content/Styles/fa-solid.css",
-                      "~/Content/Styles/fontawesome.css",
-                      "~/Content/Styles/bootstrap.css"));
+                      "~/Content/fontawesome.css",
+                      "~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/ValidationPortal")
                 .Include("~/Content/Styles/validationportal.css"));
@@ -19,11 +18,13 @@ namespace ValidationWeb
             // SCRIPTS
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Content/Scripts/modernizr-*"));
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Content/Scripts/jquery-{version}.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Content/Scripts/popper.min.js",
-                      "~/Content/Scripts/bootstrap.js",
+                      "~/Scripts/umd/popper.js",
+                      "~/Scripts/umd/popper-utils,js",
+                      "~/Scripts/bootstrap.js",
                       "~/Content/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/table2CSV").Include("~/Content/Scripts/table2csv.js"));
@@ -36,14 +37,6 @@ namespace ValidationWeb
             bundles.Add(new StyleBundle("~/Content/DataTables/css/DataTablesCSS").Include(
                 "~/Content/DataTables/css/dataTables.bootstrap4.css")); 
             
-            bundles.Add(
-                new StyleBundle("~/Content/Styles/BootstrapSelectCSS")
-                    .Include("~/Content/Styles/bootstrap-select.css"));
-
-            bundles.Add(
-                new ScriptBundle("~/bundles/BootstrapSelect")
-                    .Include("~/Scripts/bootstrap-select.js"));
-
             bundles.Add(
                 new StyleBundle("~/Content/Styles/Select2CSS")
                     .Include("~/Content/Styles/select2.css")
