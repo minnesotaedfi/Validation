379d7ce4d281daeffe526aca389a5d2d09e8f004
Pocky Southmayd <j.southmayd@wearedoubleline.com>
2019-02-07 10:09:11 -0600

    restored IE11 functionality. need to test on chrome/edge/ff and investigate missing icons

--- ValidationWeb/App_Start/BundleConfig.cs
+++ ValidationWeb/App_Start/BundleConfig.cs
@@ -17,10 +17,11 @@ namespace ValidationWeb
 
             // SCRIPTS
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Content/Scripts/modernizr-*"));
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Content/Scripts/jquery-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Content/Scripts/jquery-{version}.js",
+                "~/Scripts/url-search-params-polyfill.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/umd/popper.js",
                       "~/Scripts/umd/popper-utils,js",
