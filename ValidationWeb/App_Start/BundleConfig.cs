e48328ec4911ed26595d8955335062483373597c
Pocky Southmayd <j.southmayd@wearedoubleline.com>
2019-02-13 17:23:17 -0600

    getting jqueryUI autocomplete control for validation report table filters

--- ValidationWeb/App_Start/BundleConfig.cs
+++ ValidationWeb/App_Start/BundleConfig.cs
@@ -30,8 +30,8 @@ namespace ValidationWeb
                       "~/Content/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/table2CSV").Include("~/Content/Scripts/table2csv.js"));
-            bundles.Add(new ScriptBundle("~/bundles/jqueryUI").Include("~/Content/Scripts/jquery-ui.js"));
-            bundles.Add(new StyleBundle("~/Content/jqueryUICSS").Include("~/Content/Styles/jquery-ui.css"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryUI").Include("~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new StyleBundle("~/Content/jqueryUICSS").Include("~/Content/themes/base/jquery-ui.css"));
 
             bundles.Add(new StyleBundle("~/Content/datePickerCSS").Include("~/Content/Styles/bootstrap-datetimepicker.min.css"));
             bundles.Add(new ScriptBundle("~/bundles/datePickerJS").Include("~/Content/Scripts/moment.js", "~/Content/Scripts/bootstrap-datetimepicker.min.js"));
