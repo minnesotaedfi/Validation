f4aef019180416837cff41f100568d0f073a6dca
Pocky Southmayd <j.southmayd@wearedoubleline.com>
2019-08-25 20:48:45 -0500

    updating admin role name

--- Validation.DataModels/SSORoleNames.cs
+++ Validation.DataModels/SSORoleNames.cs
@@ -2,7 +2,7 @@
 {
     public class SSORoleNames
     {
-        public const string Admin = "EDVP-Administrator";
+        public const string Admin = "EDVP-Admin";
         public const string HelpDesk = "EDVP-HelpDesk";
         public const string RegionUser = "EDVP-RegionUser";
         public const string DistrictUser = "EDVP-DistrictUser";
