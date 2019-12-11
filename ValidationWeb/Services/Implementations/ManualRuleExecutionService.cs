b9346777da766714e3529c123e727f9f5caeb671
Salvador Peralta <salvador@nearshoredevs.com>
2019-12-11 15:19:14 -0600

    Adding extra log for Manual SQL

--- ValidationWeb/Services/Implementations/ManualRuleExecutionService.cs
+++ ValidationWeb/Services/Implementations/ManualRuleExecutionService.cs
@@ -9,9 +9,12 @@ namespace ValidationWeb.Services.Implementations
     public class ManualRuleExecutionService: IManualRuleExecutionService
     {
         private readonly IRulesEngineConfigurationValues _rulesEngineConfigurationValues;
-        public ManualRuleExecutionService(IRulesEngineConfigurationValues rulesEngineConfigurationValues)
+        private readonly ILoggingService _loggingService;
+
+        public ManualRuleExecutionService(IRulesEngineConfigurationValues rulesEngineConfigurationValues, ILoggingService loggingService)
         {
             _rulesEngineConfigurationValues = rulesEngineConfigurationValues;
+            _loggingService = loggingService;
         }
 
         public async Task<List<string>> GetManualSqlFile(string ruleSetId, string ruleId)
@@ -19,12 +22,15 @@ namespace ValidationWeb.Services.Implementations
             var readingDirectory = $@"{_rulesEngineConfigurationValues.SqlRulesFileFolder}\{ruleSetId}";
             var manualInstructionsToExecute = new List<string>();
 
+            
             if (Directory.Exists(readingDirectory))
             {
+                _loggingService.LogInfoMessage($"Looking SQLRules for rule: {ruleId}, rules set: {ruleSetId}.");
                 foreach (var fileName in Directory.EnumerateFiles(readingDirectory))
                 {
                     if (fileName.Contains(ruleId))
                     {
+                        _loggingService.LogInfoMessage($"SQLRules founded for rule: {ruleId}, rules set: {ruleSetId}, Path: {fileName}.");
                         using (var reader = File.OpenText(fileName))
                         {
                             var fileSqlExec = await reader.ReadToEndAsync();
