9e34d6df41ad5074ceb1fd0cc39d3c5703eb20f2
steve-dlp <steve@doublelinepartners.com>
2020-12-09 11:48:51 -0600

    Create digital-equity-rule60.01.0014.sql

---
+++ ValidationWeb/Content/SqlRules/DigitalEquityRuleSet/digital-equity-rule60.01.0014.sql
@@ -0,0 +1,51 @@
+/*
+
+Error on:
+
+IF SEOA.StudentIndicator.IndicatorName = DigitalDevice and SEOA.StudentIndicator.Indicator = None, 
+error on additional record for same student where SEOA.StudentIndicator.IndicatorName = PrimaryLearningDeviceProvider
+
+*/
+
+DECLARE @RuleId VARCHAR(32) = '60.01.0011';
+DECLARE @Message NVARCHAR(MAX) = 'When Digital Device is not reported as None, PrimaryLearningDeviceProvider should not be reported for this student.
+
+If SEOA.StudentIndicator.IndicatorName = DigitalDevice and SEOA.StudentIndicator.Indicator = None, 
+error on additional record for same student where SEOA.StudentIndicator.IndicatorName = PrimaryLearningDeviceProvider';
+DECLARE @IsError BIT = 1;
+
+WITH 
+failed_rows AS (
+
+SELECT
+  DISTINCT Student.StudentUniqueId
+FROM 
+  edfi.StudentEducationOrganizationAssociationStudentIndicator
+  JOIN
+  edfi.Student
+    ON Student.StudentUSI = StudentEducationOrganizationAssociationStudentIndicator.StudentUSI
+  JOIN
+  edfi.School
+    ON School.SchoolId = StudentEducationOrganizationAssociationStudentIndicator.EducationOrganizationId
+WHERE 
+  StudentEducationOrganizationAssociationStudentIndicator.IndicatorGroup = 'DigitalEquity'
+  AND StudentEducationOrganizationAssociationStudentIndicator.IndicatorName = 'DigitalDevice'
+  AND StudentEducationOrganizationAssociationStudentIndicator.Indicator = 'None'
+  AND EXISTS (
+    SELECT 1
+    FROM
+      edfi.StudentEducationOrganizationAssociationStudentIndicator InternetPerformance
+    WHERE
+      InternetPerformance.IndicatorGroup = 'DigitalEquity'
+      AND InternetPerformance.IndicatorName = 'PrimaryLearningDeviceProvider'
+      AND InternetPerformance.EducationOrganizationId = StudentEducationOrganizationAssociationStudentIndicator.EducationOrganizationId
+      AND InternetPerformance.StudentUSI = StudentEducationOrganizationAssociationStudentIndicator.StudentUSI
+    )
+  AND School.LocalEducationAgencyId = @DistrictId
+
+)
+INSERT INTO 
+	rules.RuleValidationDetail (RuleValidationId, Id, RuleId, IsError, [Message])
+SELECT
+	@RuleValidationId, StudentUniqueId, @RuleId, @IsError, @Message
+FROM failed_rows;
