cc15d4d103e0ac3a33241abe4c1ee4dcd6bf29c8
bbullard-doubleline <50177393+bbullard-doubleline@users.noreply.github.com>
2021-03-02 14:38:49 -0600

    save

---
+++ ValidationWeb/Content/SqlRules/McccCourseRuleSet/mccc-course-rule50.02.0016.sql
@@ -0,0 +1,63 @@
+/*
+
+C is NOT a valid choice for K-8
+
+*/
+
+DECLARE @RuleId VARCHAR(32) = '50.02.0016';
+DECLARE @Message NVARCHAR(MAX) = 'Course level type combination error detected for course offered for grades K-8. C is not a valid choice.';
+DECLARE @IsError BIT = 1;
+
+WITH 
+course_level_types AS (
+
+SELECT
+ CourseLevelType.EducationOrganizationId,
+ CourseLevelType.CourseCode,
+ CourseLevel.CodeValue AS CourseLevelCodeValue
+FROM 
+ mn.CourseLevelType
+ INNER JOIN 
+ edfi.Descriptor CourseLevel
+  ON CourseLevelTypeDescriptorId = CourseLevel.DescriptorId
+ INNER JOIN
+ edfi.CourseOfferedGradeLevel
+  ON CourseOfferedGradeLevel.EducationOrganizationId = CourseLevelType.EducationOrganizationId
+   AND CourseOfferedGradeLevel.CourseCode = CourseLevelType.CourseCode
+ INNER JOIN
+ edfi.GradeLevelDescriptor
+  ON GradeLevelDescriptor.GradeLevelDescriptorId = CourseOfferedGradeLevel.GradeLevelDescriptorId
+ INNER JOIN
+ edfi.Descriptor GradeLevel
+  ON GradeLevel.DescriptorId = GradeLevelDescriptor.GradeLevelDescriptorId
+WHERE
+ CourseLevelType.EducationOrganizationId = @DistrictId
+ AND GradeLevel.CodeValue IN ('HK','KA','KB','KC','KD','KE','KF','KG','KI','KJ','KK','1','2','3','4','5','6','7','8')
+GROUP BY
+ CourseLevelType.EducationOrganizationId,
+ CourseLevelType.CourseCode,
+ CourseLevel.CodeValue
+
+ ),
+failed_rows AS (
+
+SELECT
+ DISTINCT CourseCode
+FROM
+ course_level_types
+WHERE
+ CourseLevelCodeValue = 'C'
+
+)
+INSERT INTO 
+ rules.RuleValidationDetail (RuleValidationId, Id, RuleId, IsError, [Message])
+SELECT TOP 1
+ @RuleValidationId, @DistrictId, @RuleId RuleId, @IsError IsError, 
+ @Message + CHAR(13)+CHAR(10)+ (
+  SELECT TOP 1 
+   CourseCode
+  FROM failed_rows [xml] 
+  FOR XML RAW,
+   ROOT ('Detail')
+  ) [Message]
+FROM failed_rows;
